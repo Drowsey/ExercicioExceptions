a9fe8cd8ca506131b3c27a3b48a230a1cc76e8bc
Drowsey <allenkevin.cardoso@gmail.com>
2021-08-02 21:06:19 -0300

    Adicionada as classes e textos, mas sem exceptions

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TratamentoExcecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TratamentoExcecoes.Entities;
 
 namespace TratamentoExcecoes
 {
@@ -6,7 +7,25 @@ namespace TratamentoExcecoes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Room number: ");
+            int roomNum = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation r1 = new Reservation(roomNum, checkIn, checkOut);
+            Console.Write("Reservation: " + r1);
+
+            Console.WriteLine("\n\nEnter data to update the reservation:");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            r1.UpdateDates(checkIn, checkOut);
+            Console.Write("Reservation: "+ r1 +"\n\n");
+            
         }
     }
 }
